bf555cab48543409a4e555bce24f59e2956382f7
Lisstem <thole.goesmann@gmx.de>
2018-03-21 19:59:11 +0100

    Added already written code and example

---
+++ example.rb
@@ -0,0 +1,74 @@
+require_relative 'myanimelist'
+
+# creating a new MyAnimeList instance
+# exchange 'user' and 'password' with your username and password
+list = MyAnimeList.new('user', 'password')
+
+# searching for animes
+search_string = 'Naruto'
+animes = list.search_anime(search_string)
+
+# prints all animes
+puts 'print all animes'
+animes.each do |anime|
+  puts "#{anime[:title]} (#{anime[:id]})
+\tEnglish title: #{anime[:english]}
+\tSynonyms:      #{anime[:synonyms].join(', ')}
+\tScore:         #{anime[:score]}/10
+\tType:          #{anime[:type]}
+\tEpisodes:      #{anime[:episodes]}
+\tStart date:    #{(anime[:start_date].respond_to? :strftime) ? anime[:start_date].strftime('%d %b %Y') : anime[:start_date]}
+\tEnd date:      #{(anime[:end_date].respond_to? :strftime) ? anime[:end_date].strftime('%d %b %Y') : anime[:end_date]}
+\tStatus:        #{anime[:status]}
+\tSynopsis:      #{anime[:synopsis]}
+\tImage link:    #{anime[:image]}"
+end
+
+# searching for mangas
+search_string = 'Naruto'
+animes = list.search_manga(search_string)
+
+# prints all mangas
+puts 'print all mangas'
+animes.each do |manga|
+  puts "#{manga[:title]} (#{manga[:id]})
+\tEnglish title: #{manga[:english]}
+\tSynonyms:      #{manga[:synonyms].join(', ')}
+\tScore:         #{manga[:score]}/10
+\tType:          #{manga[:type]}
+\tChapters:      #{manga[:chapters]}
+\tVolumes        #{manga[:volumes]}
+\tStart date:    #{(manga[:start_date].respond_to? :strftime) ? manga[:start_date].strftime('%d %b %Y') : manga[:start_date]}
+\tEnd date:      #{(manga[:end_date].respond_to? :strftime) ? manga[:end_date].strftime('%d %b %Y') : manga[:end_date]}
+\tStatus:        #{manga[:status]}
+\tSynopsis:      #{manga[:synopsis]}
+\tImage link:    #{manga[:image]}"
+end
+
+# adding an anime
+# first create a data hash
+data = {:episode =>10,
+        :status => 'watching',
+        :score =>7,
+        :tags => 'test tag, 2nd tag'}
+puts MyAnimeList.anime_xml(data)
+# 28999 is the id of the anime on MyAnimeList.net
+puts 'adding anime'
+puts list.add_anime(28999, data)
+
+# updating an anime
+data[:episode] += 1
+# 28999 is the id of the anime on MyAnimeList.net
+puts 'updating anime'
+puts list.update_anime(28999, data)
+
+# deleting an anime
+# 28999 is the id of the anime on MyAnimeList.net
+puts 'deleting anime'
+puts list.delete_anime(28999)
+
+# it can take MyAnimeList.net to update your stats after you changed your watch/read list
+# this cannot be changed by this Wrapper and does not mean something is broken
+
+# adding/updating/deleting a manga is mostly the same
+# for a full list of the tags see the MyAnimeList.net API documentation (https://myanimelist.net/modules.php?go=api)
---
+++ myanimelist.rb
@@ -0,0 +1,268 @@
+require 'rest-client' # see https://github.com/rest-client/rest-client
+require 'crack/xml' # see https://github.com/jnunemaker/crack/
+require 'date'
+
+# author 'Lisstem'
+#
+# Wrapper for the MyAnimeList.net API
+# See https://myanimelist.net/modules.php?go=api for more information on the API including usage and values
+#
+# please note that the password will be stored in plain text in your RAM
+class MyAnimeList
+  # Tags used in the anime XML
+  @anime_tags = [:episode, :status, :score, :storage_type, :storage_value, :times_rewatched, :rewatch_value,
+                 :date_start, :date_finish, :priority, :enable_discussion, :enable_rewatching, :comments, :tags]
+  # Tags used in the manga XML
+  @manga_tags = [:chapter, :volume, :status, :score, :times_reread, :reread_value, :date_start,:date_finish, :date,
+                 :priority, :enable_discussion, :enable_rereading, :comments, :scan_group, :tags, :retail_volumes]
+
+  # accessors for user and password
+  attr_accessor :user, :password
+
+  # Initializes new instances
+  # @param [String] user username for MyAnimeList.net
+  # @param [String] password password for MyAnimeList.net
+  # it is sufficient if user and password to_s evaluates to the name and password
+  def initialize(user, password)
+    @user = user
+    @password = password
+  end
+
+  # searches for matches of Animes for a given string
+  # @param [String] waldo String to search for (will be automatically escaped)
+  # @return [Array] returns an array of hashes of the matches for waldo
+  #   in case of a unknown status code the Restclient::Response is returned
+  #   each hash represents a match and includes the following key value pairs:
+  #     :id => integer ;id on MyAnimeList.net
+  #     :title => string (might be nil);japanese title
+  #     :english => string (might be empty string) ;english title
+  #     :synonyms => array of strings (might be empty) ;each string is a synonym
+  #     :episodes => integer ;count of episodes
+  #     :score => float ;score on MyAnimeList.net
+  #     :type => symbol ;type of the anime as symbol
+  #     :status => string ;status of the anime i.e. 'Finished Airing'
+  #     :start_date => Date or String if unconvertable ;start of airing
+  #     :end_date => Date or String if unconvertable ;end of airing
+  #     :synopsis => string ;synopsis
+  #     :image => string (might be nil) ;link to the image on MyAnimeList.net
+  def search_anime(waldo)
+    response = RestClient.get "https://#{user}:#{password}@myanimelist.net/api/anime/search.xml?q=#{CGI::escapeHTML(waldo)}"
+    case response.code
+      when 200
+        return MyAnimeList.create_animes response.body
+      when 204
+        return []
+      else
+        return response
+    end
+  end
+
+  # adds a manga to the readlist
+  # @param [Integer] id id of the anime on MyAnimeList.net
+  # @param [Hash] data hash with the data of the read anime
+  #   The hash must contain the symbols in @anime_tags missing tags
+  #   missing values will be added as nil
+  #   some values must be present
+  #   for further information check the MyAnimeList API site
+  # @return [Object] the result of RestClient.post
+  def add_anime(id, data)
+    RestClient.post "https://#{user}:#{password}@myanimelist.net/api/animelist/add/#{id}.xml",
+                    {:data => MyAnimeList.anime_xml(data)}
+  end
+
+  # updates an anime on the watchlist
+  # @param [Integer] id id of the anime on MyAnimeList.net
+  # @param [Hash] data hash with the data of the read anime
+  #   The hash must contain the symbols in @anime_tags missing tags
+  #   missing values will be added as nil
+  #   some values must be present
+  #   for further information check the MyAnimeList API site
+  # @return [Object] the result of RestClient.post
+  def update_anime(id, data)
+    RestClient.post "https://#{user}:#{password}@myanimelist.net/api/animelist/update/#{id}.xml",
+                    {:data => MyAnimeList.anime_xml(data)}
+  end
+
+  # deletes an anime from the watchlist
+  # @param [Integer] id id of the anime on MyAnimeList.net
+  # @return [Object] the result of RestClient.delete
+  def delete_anime(id)
+    RestClient.delete "https://#{user}:#{password}@myanimelist.net/api/animelist/delete/#{id}.xml"
+  end
+
+  # searches for matches of Mangas for a given string
+  # @param [String] waldo String to search for (will be automatically escaped)
+  # @return [Array of Hashes] returns an array of hashes of the matches for waldo
+  #   in case of a unknown status code the Restclient::Response is returned
+  #   each hash represents a match and includes the following key value pairs:
+  #     :id => integer ;id on MyAnimeList.net
+  #     :title => string ;japanese title
+  #     :english => string (might be empty string) ;english title
+  #     :synonyms => array of strings (might be empty) ;each string is a synonym
+  #     :chapters => integer ;count of chapters
+  #     :volumes = integer ;count of english volumes
+  #     :score => float ;score on MyAnimeList.net
+  #     :type => symbol ;type of the anime as symbol
+  #     :status => string ;status of the anime i.e. 'Finished Airing'
+  #     :start_date => Date or String if unconvertable ;start of airing
+  #     :end_date => Date or String if unconvertable ;end of airing
+  #     :synopsis => string ;synopsis
+  #     :image => string (might be nil) ;link to the image on MyAnimeList.net
+  def search_manga(waldo)
+    response = RestClient.get "https://#{user}:#{password}@myanimelist.net/api/manga/search.xml?q=#{CGI::escapeHTML(waldo)}"
+    case response.code
+      when 200
+        return MyAnimeList.create_mangas response.body
+      when 204
+        return []
+      else
+        return response
+    end
+  end
+
+  # adds a manga to the readlist
+  # @param [Integer] id id of the manga on MyAnimeList.net
+  # @param [Hash] data hash with the data of the read manga
+  #   The hash must contain the symbols in @manga_tags missing tags
+  #   missing values will be added as nil
+  #   some values must be present
+  #   for further information check the MyAnimeList API site
+  # @return [Object] the result of RestClient.post
+  def add_manga(id, data)
+    RestClient.post "https://#{user}:#{password}@myanimelist.net/api/mangalist/add/#{id}.xml",
+                    {:data => MyAnimeList.manga_xml(data)}
+  end
+
+  # updates a manga on the readlist
+  # @param [Integer] id id of the manga on MyAnimeList.net
+  # @param [Hash] data hash with the data of the read manga
+  #   The hash must contain the symbols in @manga_tags missing tags
+  #   missing values will be added as nil
+  #   some values must be present
+  #   for further information check the MyAnimeList API site
+  # @return [Object] the result of RestClient.post
+  def update_manga(id, data)
+    RestClient.post "https://#{user}:#{password}@myanimelist.net/api/mangalist/update/#{id}.xml",
+                    {:data => MyAnimeList.manga_xml(data)}
+  end
+
+  # deletes a manga from the readlist
+  # @param [Integer] id id of the manga on MyAnimeList.net
+  # @return [Object] the result of RestClient.delete
+  def delete_manga(id)
+    RestClient.delete "https://#{user}:#{password}@myanimelist.net/api/mangalist/delete/#{id}.xml"
+  end
+
+  # creates the anime XML string for MyAnimeList.net for a data hash
+  # @param [Hash] data the data hash
+  # @return [String] the XML string
+  def self.anime_xml(data)
+    create_xml(@anime_tags, data)
+  end
+
+  # creates the manga XML string for MyAnimeList.net for a data hash
+  # @param [Hash] data the data hash
+  # @return [String] the XML string
+  def self.manga_xml(data)
+    create_xml(@manga_tags, data)
+  end
+
+  # creates a XML string given a set of tags and data hash
+  # @param [Array] tags the tags
+  # @param [Hash] data the data hash
+  # @return [String] the XML string
+  def self.create_xml(tags, data)
+    xml = '<?xml version="1.0" encoding="UTF-8"?>'
+    xml << "\n<entry>\n"
+    tags.each do |tag|
+      value = data[tag]
+      value = '' if value.nil?
+      value = value.join(',') if value.respond_to? :each
+      xml << "  <#{tag.to_s}>#{value}</#{tag.to_s}>\n"
+    end
+    xml << "</entry>"
+  end
+
+  # Parses the XML response to a Array of Hashes representing the animes
+  # @param [String] xml the XML response as string
+  # @return [Array] the Array of Hashes
+  def self.create_animes(xml)
+    animes = []
+    xml = Crack::XML.parse(xml)['anime']['entry']
+    if xml.respond_to? :each_pair
+      xml = [xml]
+    end
+    xml.each do |entry|
+      anime = {}
+      anime[:id] = entry['id'].to_i
+      anime[:title] = entry['title']
+      anime[:english] = entry['english']
+      anime[:synonyms] = entry['synonyms']
+      if anime[:synonyms].nil?
+        anime[:synonyms] = []
+      else
+        anime[:synonyms] = anime[:synonyms].split(';')
+      end
+      anime[:episodes] = entry['episodes'].to_i
+      anime[:score] = entry['score'].to_f
+      anime[:type] = entry['type'].downcase.to_sym
+      anime[:status] = entry['status']
+      begin
+        anime[:start_date] = Date.parse(entry['start_date'])
+      rescue ArgumentError
+        anime[:start_date] = entry['start_date']
+      end
+      begin
+        anime[:end_date] = Date.parse(entry['end_date'])
+      rescue ArgumentError
+        anime[:end_date] = entry['end_date']
+      end
+      anime[:synopsis] = entry['synopsis']
+      anime[:image] = entry['image']
+      animes << anime
+    end
+    animes
+  end
+
+  # Parses the XML response to a Array of Hashes representing the mangas
+  # @param [String] xml the XML response as string
+  # @return [Array] the Array of Hashes
+  def self.create_mangas(xml)
+    mangas = []
+    xml = Crack::XML.parse(xml)['manga']['entry']
+    if xml.respond_to? :each_pair
+      xml = [xml]
+    end
+    xml.each do |entry|
+      manga = {}
+      puts entry
+      manga[:id] = entry['id'].to_i
+      manga[:title] = entry['title']
+      manga[:english] = entry['english']
+      if manga[:synonyms].nil?
+        manga[:synonyms] = []
+      else
+        manga[:synonyms] = manga[:synonyms].split(';')
+      end
+      manga[:chapters] = entry['chapters'].to_i
+      manga[:volumes] = entry['volumes'].to_i
+      manga[:score] = entry['score'].to_f
+      manga[:type] = entry['type'].downcase.to_sym
+      manga[:status] = entry['status']
+      begin
+        manga[:start_date] = Date.parse(entry['start_date'])
+      rescue ArgumentError
+        manga[:start_date] = entry['start_date']
+      end
+      begin
+        manga[:end_date] = Date.parse(entry['end_date'])
+      rescue ArgumentError
+        manga[:end_date] = entry['end_date']
+      end
+      manga[:synopsis] = entry['synopsis']
+      manga[:image] = entry['image']
+      mangas << manga
+    end
+    mangas
+  end
+end
